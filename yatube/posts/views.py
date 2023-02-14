f5f31048552258beb19b0ca33098bd46c8d8ae42
Виталий Донцов <anon-44@yandex.ru>
2023-02-14 23:29:29 +0300

    создал template и index.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,17 +3,13 @@ from django.http import HttpResponse
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template) 
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
     return HttpResponse('не главная')
 
-"""
-# Страница с информацией об одном сорте мороженого;
-# view-функция принимает параметр pk из path()
-def ice_cream_detail(request, pk):
-    return HttpResponse(f'Мороженое номер {pk}') 
-"""
+
