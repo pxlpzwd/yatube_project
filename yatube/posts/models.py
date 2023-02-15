7effe70246849af1c82a9be744463bdf88414333
Виталий Донцов <anon-44@yandex.ru>
2023-02-15 21:53:18 +0300

    комент

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,8 +1,15 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     text = models.TextField()
@@ -10,5 +17,11 @@ class Post(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name="posts"
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Group, Post
 
 # Главная страница
 def index(request):
     template = 'posts/index.html'
     title = "Это главная страница проекта Yatube"
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
+        'posts': posts,
         'title': title,
         'text': 'Последние обновления на сайте'
     }
@@ -17,10 +18,13 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     title = "Здесь будет информация о группах проекта Yatube"
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'
     context = {
+        'group': group,
+        'posts': posts,
         'title': title,
-        'text': 'Лев Толстой – зеркало русской революции.!!11!'
+        'text': 'Лев Толстой – зеркало русской революции.!!11!' 
     }
     return render(request, template, context)
-
-
