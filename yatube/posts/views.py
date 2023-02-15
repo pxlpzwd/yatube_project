a2d9b485b7bd6caf5526c52551107a70f0431e5a
Виталий Донцов <anon-44@yandex.ru>
2023-02-15 09:51:24 +0300

    настроил template и context, добавил title и text в шаблон

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,12 +5,22 @@ from django.http import HttpResponse
 # Главная страница
 def index(request):
     template = 'posts/index.html'
-    return render(request, template) 
+    title = "Это главная страница проекта Yatube"
+    context = {
+        'title': title,
+        'text': 'Последние обновления на сайте'
+    }
+    return render(request, template, context) 
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title': title,
+        'text': 'Лев Толстой – зеркало русской революции.!!11!'
+    }
+    return render(request, template, context)
 
 
